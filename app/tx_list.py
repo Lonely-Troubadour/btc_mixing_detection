4fcd3c4b3519e53dc2390a21ff2ae45950bb6873
Mr.Who <3062618707@qq.com>
2021-04-19 11:24:52 +0800

    Add react app

--- app/tx_list.py
+++ app/tx_list.py
@@ -6,17 +6,17 @@ bp = Blueprint('txs', __name__, url_prefix='/txs')
 
 @bp.route('/fe', methods=('GET', 'POST'))
 def fe():
-    return render_template('transactions/list.html', tx_type='fe')
+    return render_template('transactions/list.html', tx_type='Fair Exchange')
 
 
 @bp.route('/coinswap', methods=('GET', 'POST'))
 def coinswap():
-    return render_template('transactions/list.html', tx_type='coinswap')
+    return render_template('transactions/list.html', tx_type='CoinSwap')
 
 @bp.route('/coinjoin', methods=('GET', 'POST'))
 def coinjoin():
-    return render_template('transactions/list.html', tx_type='coinjoin')
+    return render_template('transactions/list.html', tx_type='CoinJoin')
 
 @bp.route('/sa', methods=('GET', 'POST'))
 def sa():
-    return render_template('transactions/list.html', tx_type='sa')
+    return render_template('transactions/list.html', tx_type='Stealth Address')
